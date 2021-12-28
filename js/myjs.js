533d92efa702bfbfc50e6edd36a0d92f9cb922a2
Twikura <rainbowtrash2333@gmail.com>
2021-12-28 17:03:11 +0800

    下一步,写一个所有内容的json

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,22 @@
-$(document).ready(function(){
-    $(".hide-btn").click(function(){
+//hide add
+function hide_add(){
+    $(".hide-btn").click(function () {
         // window.alert("test")
         $(this).next().hide();
         $(this).hide();
     });
-});
+}
+// mm-dd
+function getDate(date) {
+    return ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '-' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate()));
+}
+function getArtistsListHtml(Artistlist){
+    var html = "";
+    Artistlist.forEach(function (element) {
+        html += "                       <a href=\"" + element.href +
+            "\" class=\"list-group-item \" style=\"font-size: 16px\">" +
+            "<span class=\"badge badge-primary badge-date\">" + getDate(element.date) +
+            "</span>\n" + element.name + "\n" + " </a>\n"
+    })
+    return html
+}
